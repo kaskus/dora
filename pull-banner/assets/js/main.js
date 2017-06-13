cf182a24e5946a2fadf32f5212a63527d46f8d4d
timothykevin <timotius.kevin@gdplabs.id>
2017-06-13 13:26:40 +0700

    impl pull banner

--- pull-banner/assets/js/main.js
+++ pull-banner/assets/js/main.js
@@ -1,10 +1,14 @@
 window.addEventListener('message', function(e) {
   var message = e.data.message;
   switch (message) {
-    case 'expand-banner':
-      $('#banner-frame').addClass('stretched'); break;
-    case 'shrink-banner':
-      $('#banner-frame').removeClass('stretched'); break;
+    case 'remove-banner':
+      $('#pull-banner').remove();
+      $(document.body).css('overflow', 'auto');
+      break;
+    case 'pull-banner':
+      $('#pull-banner').height('100%');
+      $(document.body).css('overflow', 'hidden');
+      break;
     default:
       break;
   }
