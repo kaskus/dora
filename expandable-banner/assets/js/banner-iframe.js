fe223f45fe6240b032e7e29386e1b589fe4089a8
bradita <bradita.sulistianingdyar@kaskusnetworks.com>
2017-08-10 12:24:27 +0700

    add iframe,youtube videontemplate

--- expandable-banner/assets/js/banner-iframe.js
+++ expandable-banner/assets/js/banner-iframe.js
@@ -1,28 +1,24 @@
 $(document).ready(function() {
   var urlImageCondensed = 'http://i0.sinaimg.cn/fashion/2016/0107/U3978P1503DT20160107145544.gif';
   var urlImageExpanded = 'https://68.media.tumblr.com/8deca5058414d741af9c7b3674be440f/tumblr_obujcnB3kM1vy286ao1_500.gif';
+
   var urlVideoPoster = 'http://camendesign.com/code/video_for_everybody/poster.jpg';
 
   $('#banner-condensed-bg').css('background-image', 'url(' + urlImageCondensed + ')');
   $('#banner-expanded-bg').css('background-image', 'url(' + urlImageExpanded + ')');
 
   $('#banner-video').attr('poster', urlVideoPoster);
-  
-  
-
-
 });
 
 function expandBanner(e) {
+  var urlVideos = 'https://www.youtube.com/embed/WHdYxRHkMfI?autoplay=1';
   window.parent.postMessage({message: 'expand-banner'}, '*')
-
-  var urlVideo = 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4';
   $('#banner-content').addClass('stretched');
   $('#banner-video').show();
-  $('#banner-video').attr('src', urlVideo);
-  $('#banner-video').get(0).currentTime = 0;
-  $('#close-button').show();
 
+  //for iframe
+  $('#banner-video').attr('src', urlVideos);
+  $('#close-button').show();
 }
 
 function shrinkBanner(e) {
