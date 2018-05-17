aca8c567bcce7a0ed419208fcecbebd364548c52
babono <nurul.akbar@kaskusnetworks.com>
2018-05-17 12:00:01 +0700

    update underlay banner

---
+++ ggmild-underlay/assets/js/min/scroll-min.js
@@ -0,0 +1,1 @@
+$(window).bind("scroll",function(){var e=$(".notice-app").height();$(window).scrollTop()>e?($("#site-header").addClass("fixed"),$(".mobile-menu-wrapper").addClass("fixed")):($("#site-header").removeClass("fixed"),$(".mobile-menu-wrapper").removeClass("fixed"))});
